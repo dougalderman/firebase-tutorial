01fb0a6a7b2dab1d8a373051fec32c8ccfaf672e
Doug Alderman <agnadcp-d1@yahoo.com>
2016-01-02 16:26:37 -0700

    1/2/16 changes

--- home/home.js
+++ home/home.js
@@ -1,7 +1,6 @@
 'use strict';
 
-angular.module('myApp.home', ['ngRoute'])
-
+angular.module('myApp.home', ['ngRoute', 'firebase'])
 // Declared route
 .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/home', {
@@ -11,6 +10,28 @@ angular.module('myApp.home', ['ngRoute'])
 }])
 
 // Home controller
-.controller('HomeCtrl', [function() {
+.controller('HomeCtrl', ['$scope', '$firebaseSimpleLogin', function($scope, $firebaseSimpleLogin) {
+  $scope.SignIn = function(event) {
+    event.preventDefault(); // To prevent from refresh
+    var username = $scope.user.email;
+    var password = $scope.user.password;
 
+    loginObj.$login('password', {
+      email: username,
+      password: password
+    })
+    .then (function(user) {
+      // Success callback
+      console.log('Authentication successful');
+      alert('Successful login')
+    }, function(error) {
+      // Failure callback
+      console.log('Authentication failure');
+      console.log(error)
+      alert('Failed login--please try again');
+    })
+  };
+
+  var firebaseObj = new Firebase("https://blinding-inferno-3021.firebaseio.com")
+  var loginObj = $firebaseSimpleLogin(firebaseObj);
 }]);
